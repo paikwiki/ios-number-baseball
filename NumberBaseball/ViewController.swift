cbaf0e939560cdf667fe66135a3402957a35f735
paikwiki <paikwiki@gmail.com>
2020-04-29 22:19:22 +0900

    feat: Create main storyboard(draft)

--- NumberBaseball/ViewController.swift
+++ NumberBaseball/ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var answer: [Int] = [-1, -1, -1]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
