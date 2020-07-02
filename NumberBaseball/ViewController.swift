440203b6abecd98cb6be5a62f4cf4141216b50ce
paikwiki <paikwiki@gmail.com>
2020-07-02 17:17:54 +0900

    Create an outlet(answerLabel) and an action(touchKeypad)

--- NumberBaseball/ViewController.swift
+++ NumberBaseball/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var answerLabel: UILabel!
+
     private var answer: [Int] = [-1, -1, -1]
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchKeypad(_ sender: UIButton) {
+        print(sender.currentTitle!);
     }
 }
 
